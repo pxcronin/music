63cece9f56abd3e2753f24149174bb938a5e9a95
Patrick Cronin <pxcronin@gmail.com>
2021-04-13 15:46:33 -0400

    All monthly playlists added with collapsible headers working. May reorganzie into years that expand into months to make it easier to navigate

--- app/javascript/packs/collapsible.js
+++ app/javascript/packs/collapsible.js
@@ -4,8 +4,8 @@ var i;
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    // console.log(this.childNodes);
-    var content = this.childNodes[3];
+    console.log(this.childNodes);
+    var content = this.childNodes[1];
     if (content.style.display === "block") {
       content.style.display = "none";
     } else {

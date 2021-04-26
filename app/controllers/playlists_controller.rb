aa89f63472165276e23d83b9ded5cdff33ed8b71
Patrick Cronin <pxcronin@gmail.com>
2021-04-26 15:28:33 -0400

    Rearranged monthly playlist page into individual years to prevent embedded players from crashing due to sending too many requests. Added intro text for each year's page and hid the submissions page until that's up and running

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -10,4 +10,22 @@ class PlaylistsController < ApplicationController
 
   def submission
   end
+
+  def sixteen
+  end
+
+  def seventeen
+  end
+
+  def eighteen
+  end
+
+  def nineteen
+  end
+
+  def twenty
+  end
+
+  def twenty_one
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
   get '/playlists/monthly_playlist', to: 'playlists#monthly_playlist'
   get '/playlists/special_playlist', to: 'playlists#special_playlist'
   get '/playlists/submission', to: 'playlists#submission'
+  get '/playlists/twenty_one', to: 'playlists#twenty_one'
+  get '/playlists/twenty', to: 'playlists#twenty'
+  get '/playlists/nineteen', to: 'playlists#nineteen'
+  get '/playlists/eighteen', to: 'playlists#eighteen'
+  get '/playlists/seventeen', to: 'playlists#seventeen'
+  get '/playlists/sixteen', to: 'playlists#sixteen'
 end
